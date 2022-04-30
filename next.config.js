2e74704fea325e63c929c195cd3a74e7898456cd
Debaditya Bhar <bhar.debaditya@gmail.com>
2022-04-30 14:13:53 +0530

    1.1 - Created initial Stucture of the blog.
    	1. Configure TailwindCSS
    	2. Created Favicon.
    	3. Install all the npm packages for Blog next API.
    	4. Created initial version of README.md file.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+
+  basePath: "/Responsive-Navbar",
+  assetPrefix: "/Responsive-Navbar",
+};
+
+module.exports = nextConfig;
